c4bb523637f61a04ca3b83366883f8546cd46960
Nagbhushan Hegde <9343257976ab@gmail.com>
2025-08-11 16:32:59 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,7 @@
+
 import axios from "axios";
 
-// This is the correct, full URL for your backend API.
-// It should be used in both development and production.
-const BASE_URL = "https://zapp-test-api.onrender.com/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
